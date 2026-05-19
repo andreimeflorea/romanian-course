764bc566237730fdca6eb1c244ee9b9eb4b32087
andreimeflorea <andreiflorea993@gmail.com>
2026-05-19 09:10:31 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "andreimeflorea.github.io/romanian-course",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
